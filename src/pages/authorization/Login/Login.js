e108e3599105e4c51c3a06df4001f8ed8a90d024
mervenuryazanel <mervenuryazanel@gmail.com>
2022-05-04 15:14:45 +0300

    create formik base

--- src/pages/authorization/Login/Login.js
+++ src/pages/authorization/Login/Login.js
@@ -1,28 +1,50 @@
 import React from 'react';
-import { TouchableOpacity, Text, View, Dimensions} from 'react-native';
+import { TouchableOpacity, Text, View, Dimensions } from 'react-native';
 import styles from './Login.style';
 
 import Button from '../../../components/Button'
 import Input from '../../../components/Input'
 import mainStyles from '../../main_styles/main.styles';
+import { Formik } from 'formik';
 
+const initialFormValues = {
+    email: '',
+    password: '',
+}
 
 function Login({ text, onPress, navigation }) {
-    function handleSubmit() {
+    function goSignUp() {
         navigation.navigate('SignUpPage');
     }
+
+    function handleFormSubmit(formValues) {
+        console.log(formValues);
+    }
+
+
     return (
         <View style={[styles.container]}>
-            
-         
-                <View style={{ justifyContent: "center", alignItems: "center"}}>
-                    <Text style={mainStyles.header1}>Hello Again!</Text>
-                    <Text style={mainStyles.greeting}>It is nice to see you here!</Text>
-                </View>
-                
+
+
+            <View style={{ justifyContent: "center", alignItems: "center" }}>
+                <Text style={mainStyles.header1}>Hello Again!</Text>
+                <Text style={mainStyles.greeting}>It is nice to see you here!</Text>
+            </View>
+
+            <Formik initialValues={initialFormValues} onSubmit={handleFormSubmit}>
+                {({values, handleChange, handleSubmit})=>(
+                    <>
                         <View>
-                            <Input placeHolder={"enter email..."} style={{ marginTop: 400 }} />
-                            <Input placeHolder={"enter password..."} />
+                            <Input
+                                onChangeText={handleChange('email')}
+                                value={values.email}
+                                placeHolder={"enter email..."}
+                                style={{ marginTop: 400 }}
+                            />
+                            <Input
+                                onChangeText={handleChange('password')}
+                                value={values.password}
+                                placeHolder={"enter password..."} />
                             <TouchableOpacity>
                                 <Text style={[mainStyles.blueSentence, { marginRight: Dimensions.get('window').height / 35 }]}>I forgot my password.</Text>
                             </TouchableOpacity>
@@ -30,12 +52,16 @@ function Login({ text, onPress, navigation }) {
                         </View>
 
                         <View >
-                            <Button text={"Sign In"} onPress={null} />
+                            <Button text={"Sign In"} onPress={handleSubmit} />
                             <View style={{ flexDirection: "row", alignItems: "center", marginTop: Dimensions.get('window').height / 50, justifyContent: "center" }}>
                                 <Text style={{ alignSelf: 'flex-end' }}>Don't you have an account? </Text>
-                                <TouchableOpacity onPress={handleSubmit}><Text style={mainStyles.blueSentence}>Sign Up!</Text></TouchableOpacity>
+                                <TouchableOpacity onPress={goSignUp}><Text style={mainStyles.blueSentence}>Sign Up!</Text></TouchableOpacity>
                             </View>
                         </View>
+                    </>
+                )}
+            </Formik>
+
         </View>
     );
 }
--- src/pages/authorization/SignUp/SignUp.js
+++ src/pages/authorization/SignUp/SignUp.js
@@ -6,36 +6,69 @@ import Button from '../../../components/Button'
 import Input from '../../../components/Input'
 import mainStyles from '../../main_styles/main.styles';
 
+import { Formik } from 'formik';
+
+const initialFormValues = {
+    username: '',
+    email: '',
+    password: '',
+}
+
 function SignUp({ text, onPress, navigation }) {
     function goSignIn() {
         navigation.navigate('LoginPage');
     }
+
+    function handleFormSubmit(formValues) {
+        console.log(formValues);
+    }
+
     return (
         <View style={[styles.container]}>
 
 
-            <View style={{ justifyContent: "center", alignItems: "center", width: Dimensions.get('window').width / 1.5}}>
+            <View style={{ justifyContent: "center", alignItems: "center", width: Dimensions.get('window').width / 1.5 }}>
                 <Text style={[mainStyles.header1, { marginTop: Dimensions.get('window').height / 500 }]}>Register Now</Text>
                 <Text style={mainStyles.greeting}>Create an account and save matched items with your skin tone!</Text>
             </View>
+            <Formik initialValues={initialFormValues} onSubmit={handleFormSubmit}>
 
-            <View>
-                <Input placeHolder={"enter username..."} style={{ marginTop: 400 }} />
-                <Input placeHolder={"enter email..."} style={{ marginTop: 400 }} />
-                <Input placeHolder={"enter password..."} />
-                
+                { ({values, handleChange, handleSubmit})=>(
+                    <>
 
-            </View>
+                        <View>
+                            <Input
+                                value={values.username}
+                                placeHolder={"enter username..."}
+                                style={{ marginTop: 400 }}
+                                onChangeText={handleChange('username')} />
+                            <Input
+                                value={values.email}
+                                placeHolder={"enter email..."}
+                                style={{ marginTop: 400 }}
+                                onChangeText={handleChange('email')}
+                            />
+                            <Input
+                                value={values.password}
+                                placeHolder={"enter password..."}
+                                onChangeText={handleChange('password')}
+                            />
+                        </View>
 
-            <View >
-                <Button text={"Sign Up"} onPress={goSignIn} />
-                <View style={{ flexDirection: "row", alignItems: "center", marginTop: Dimensions.get('window').height / 50, justifyContent: "center" }}>
-                    <Text style={{ alignSelf: 'flex-end' }}>Already have an account? </Text>
-                    <TouchableOpacity onPress={goSignIn}><Text style={mainStyles.blueSentence}>Sign In!</Text></TouchableOpacity>
-                </View>
-            </View>
+                        <View >
+                            <Button text={"Sign Up"} onPress={handleSubmit} />
+                            <View style={{ flexDirection: "row", alignItems: "center", marginTop: Dimensions.get('window').height / 50, justifyContent: "center" }}>
+                                <Text style={{ alignSelf: 'flex-end' }}>Already have an account? </Text>
+                                <TouchableOpacity onPress={goSignIn}><Text style={mainStyles.blueSentence}>Sign In!</Text></TouchableOpacity>
+                            </View>
+                        </View>
+                    </>
+                )
+                }
 
-            {/* <Button text={"Sign In"} onPress={null} theme="secondary" /> */}
+
+            </Formik>
+
 
         </View>
     );
